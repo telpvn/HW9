fe899dcff88ae97df8a17d9a67884ec20079f61d
telpvn <telpvn@mail.ru>
2024-04-29 09:23:40 +0400

    Checking whether a number is even method

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static int number = 0;
-
     public static void main(String[] args) {
         //Написать программу, в которой пользователь вводит целое число.
         //Если число четное, то печатаем true, если не четное, печатаем false
         //В методе main НЕ ДОПУСТИМО написание какого либо кода, кроме вызова методов.
         //Методы и их логику продумайте сами
-//        numberInput();
+
+        print();
+    }
+
+    private static void print() {
+        System.out.println(numberInput() % 2 == 0);
+    }
+
+    private static int numberInput() {
         System.out.println("Введите число: ");
-        number = new Scanner(System.in).nextInt();
-        System.out.println(number % 2 == 0);
+        return new Scanner(System.in).nextInt();
     }
 }
